8fc3fe6a9d3bc6f420fb3e9d6a927a7167242933
Martharuv <mruvalcabaf1900@alumno.ipn.mx>
2024-06-28 00:34:21 -0600

    cambios en webConfig

--- src/main/java/com/ipn/mx/config/WebConfig.java
+++ src/main/java/com/ipn/mx/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://adorable-taiyaki-b70edb.netlify.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
