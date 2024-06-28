9fd5eedd865bf15078c552bb2d80e71e4768ba98
Martharuv <mruvalcabaf1900@alumno.ipn.mx>
2024-06-28 01:56:31 -0600

    cambios para generar swagger3.1

--- src/main/java/com/ipn/mx/config/WebConfig.java
+++ src/main/java/com/ipn/mx/config/WebConfig.java
@@ -1,17 +1,26 @@
 package com.ipn.mx.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://categoriayproducto-angular.netlify.app")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-        		.allowCredentials(true);
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://categoriayproducto-angular.netlify.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
+
+
